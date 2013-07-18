29f70bd8a4aef293ed5fd0e27da11c2a066318d0
Narayan Balasubramanian <bv.narayan@gmail.com>
2013-07-18 23:59:06 +0000

    modified code to use buffer

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer');
+
+var out = buf.toString(fs.readFileSync('index.html'))
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
